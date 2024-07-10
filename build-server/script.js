91327a84e4b9c2bc78a7862e6571b18f1c0201e2
Raj2604 <rajmandhare26@gmail.com>
2024-07-10 21:39:02 +0530

    build-server-test

--- build-server/script.js
+++ build-server/script.js
@@ -1,10 +1,11 @@
 const {exec} = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
 const {S3Client,PutObjectCommand} = require('@aws-sdk/client-s3');
 const mime = require('mime-types');
 const { env } = require('process');
-const S3Client = new S3Client({
+const s3Client = new S3Client({
     region: 'ap-south-1',
     credentials:{
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -19,29 +20,30 @@ async function init() {
     const outDirPath = path.join(__dirname, 'output')
     const p = exec(`cd ${outDirPath} && npm install && npm run build`)
     p.stdout.on('data', (data) => {
-        console.log(data.tostring())
+        console.log(data.toString())
     })
     p.stdout.on('error', (data) => {
-        console.error('Error',data.tostring())
+        console.error('Error',data.toString())
     })
     p.on('close', async () => {
         console.log(`Build completed`);
         const distFolderPath = path.join(__dirname,'output','dist')
         const distFolderContents = fs.readdirSync(distFolderPath,{recursive:true})
 
-        for(const filePath of distFolderContents){
+        for(const file of distFolderContents){
+            const filePath = path.join(distFolderPath,file)
             if(fs.lstatSync(filePath).isDirectory()){
                 continue;
             }
             console.log('uploading',filePath)
             const command = new PutObjectCommand({
-                Bucket: 'vercel-clone-26',
+                Bucket: 'vercel-clone26',
                 Key: `__outputs/${PROJECT_ID}/${filePath}`,
                 Body: fs.createReadStream(filePath),
                 ContentType: mime.lookup(filePath),
             })
 
-            await S3Client.send(command)
+            await s3Client.send(command)
             console.log('Uploaded',filePath)
         }
         console.log('Done...')
